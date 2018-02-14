409e546ee97216ab78123b002027ac6165121a8a
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-14 16:37:46 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,46 @@
 import discord
-import sys
 import os
 import io
+import traceback
+import sys
+import time
+import datetime
+import asyncio
+import random
+import aiohttp
+import pip
+import random
+import textwrap
+from contextlib import redirect_stdout
 from discord.ext import commands
-bot = commands.Bot(command_prefix=('_'),description="A bot made by TheEmperor™\n\nHelp Commands",owner_id=[250674147980607488])
+import json
+from discord.ext import commands
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('='),description="Zerkun Design's Discord bot.\n\nHelp Commands",owner_id=277981712989028353)
+
+
+def cleanup_code(content):
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    return content.strip('` \n')
 
 
 @bot.event
 async def on_ready():
     print('Bot is online, and ready to ROLL!')
-    while True:
-        await bot.change_presence(game=discord.Game(name=f"with {len(bot.guilds)} servers"))
-        await asyncio.sleep(15)
-        await bot.change_presence(game=discord.Game(name="_help!"))
-        await asyncio.sleep(15)
-        
-        
+    await bot.change_presence(game=discord.Game(name="_help"))
+    
+    
 @bot.command()
-async def invite(ctx):
-    """Hit me up in your sever!"""
-    await ctx.send("Lemme join that sever:https://discordapp.com/api/oauth2/authorize?client_id=413122600848326676&permissions=0&scope=bot")
-          
-
-@bot.command()
-async def support(ctx):
-  """Joins my support server"""
-  await ctx.send("Join my support server: https://discord.gg/WewwYV5")
-  
-  
+async def ping(ctx):
+    """Gives you a websocket latency."""
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='Pong! Websocket Latency:')
+    em.description = f"{bot.latency * 1000:.4f} ms"
+    await ctx.send(embed=em)
+    
+    
 if not os.environ.get('TOKEN'):
-    print("no token found REEEE!")
+   print("no token found REEEE!")
 bot.run(os.environ.get('TOKEN').strip('"'))
