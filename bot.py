9d075f43706d3906bc23c083f8a58162fe4d8f79
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-13 19:03:32 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+import discord
+import sys
+import os
+import io
+from discord.ext import commands
+bot = commands.Bot(command_prefix=('_'),description="A bot made by TheEmperor™\n\nHelp Commands",owner_id=[250674147980607488])
+
