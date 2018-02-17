435812b4fe10e7615ab57b3fc56f8d49687bb67d
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-17 10:39:51 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,11 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('_'),description="T
 @bot.event
 async def on_ready():
     print('Bot is online, and ready to ROLL!')
-    await bot.change_presence(game=discord.Game(name="_help"))
+    while True:
+        await bot.change_presence(game=discord.Game(name=f"with {len(bot.guilds)} servers!"))
+        await asyncio.sleep(15)
+        await bot.change_presence(game=discord.Game(name="_help"))
+        await asyncio.sleep(15)
     
     
 @bot.command()
@@ -38,7 +42,7 @@ async def invite(ctx):
     await ctx.send ("Lemme join dat club: https://discordapp.com/api/oauth2/authorize?client_id=414456650519412747&permissions=0&scope=bot")
     
     
-    @bot.command()
+@bot.command()
 @commands.has_permissions(manage_messages = True)
 async def purge(ctx, num: int):
     """Deletes msgs. _purge [how many msgs].""" 
@@ -54,6 +58,7 @@ async def purge(ctx, num: int):
             await ctx.send("Done :ok_hand:")
     except discord.Forbidden:
         await ctx.send("Unable to purge. I don't have Manage Messages permission.")
+
     
 if not os.environ.get('TOKEN'):
    print("no token found REEEE!")
