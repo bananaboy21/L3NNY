b73b32426037562b22c5b015ce20224b7c376bcc
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-18 18:04:05 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,8 @@ async def invite(ctx):
     
  @bot.command()
 async def upvote(ctx):
-    await ctx.send (" Upvote me here! https://discordbots.org/bot/414456650519412747") 
+    """Upvote me!"""
+    await ctx.send ("Upvote me here! https://discordbots.org/bot/414456650519412747") 
     
 @bot.command()
 @commands.has_permissions(manage_messages = True)
