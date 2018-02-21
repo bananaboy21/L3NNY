dd96e0a62b68e4005e9cb9213fab1845732ec99b
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-20 19:54:28 -0600

    Create dm.py

---
+++ cogs/dm.py
@@ -0,0 +1,12 @@
+    @commands.command()
+    @commands.has_permissions(administrator = True)
+    async def dm(self, ctx, user: discord.Member, *, msg: str):
+        """DM someone as me!"""
+        try:
+            await user.send(msg)
+            await ctx.message.delete()            
+            await ctx.send("They got the message.:ok_hand:")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("rip you. you dont got enough permz..")
+        except:
+            await ctx.send(":x: Error. Make sure your message is shaped in this way: _dm [tag person] [msg]")
