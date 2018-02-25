1a9101e667f56ad73ed0564233cc9bc05661843f
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-25 15:39:17 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,7 +128,29 @@ async def ban(ctx, user: discord.Member):
 async def github(ctx):
     """Get my github repo"""
     await ctx.send("Here is my github: http://bit.ly/2ogUv2T")
+
     
+    @commands.command(aliases=['av'])
+    async def avatar(self, ctx, user: discord.Member = None):
+        """Returns a user's avatar url. Use *av [user], or just *av for your own."""
+        if user is None:
+            av = ctx.message.author.avatar_url
+            if '.gif' in av:
+                av += "&f=.gif"
+            color = discord.Color(value=0x00ff00)
+            em = discord.Embed(color=color, title=ctx.message.author.name)
+            em.set_author(name='Profile Picture')
+            em.set_image(url=av)
+            await ctx.send(embed=em)                  
+        else:
+            av = user.avatar_url
+            if '.gif' in av:
+                av += "&f=.gif"
+            color = discord.Color(value=0xffffff)
+            em = discord.Embed(color=color, title=user.name)
+            em.set_author(name='Profile Picture')
+            em.set_image(url=av)
+            await ctx.send(embed=em)    
     
 if not os.environ.get('TOKEN'):
    print("no token found REEEE!")
