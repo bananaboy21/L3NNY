8d1293e7944d9371fef4c2cf6905304d13747b81
TheEmperorThatCodes <35822194+TheEmporor@users.noreply.github.com>
2018-02-25 15:42:53 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,14 +132,14 @@ async def github(ctx):
     
     @commands.command(aliases=['av'])
     async def avatar(self, ctx, user: discord.Member = None):
-        """Returns a user's avatar url. Use *av [user], or just *av for your own."""
+        """Gives you a avatar"""
         if user is None:
             av = ctx.message.author.avatar_url
             if '.gif' in av:
                 av += "&f=.gif"
-            color = discord.Color(value=0x00ff00)
+            color = discord.Color(value=0xffffff)
             em = discord.Embed(color=color, title=ctx.message.author.name)
-            em.set_author(name='Profile Picture')
+            em.set_author(name='Profile Picture (pfp)')
             em.set_image(url=av)
             await ctx.send(embed=em)                  
         else:
@@ -150,7 +150,8 @@ async def github(ctx):
             em = discord.Embed(color=color, title=user.name)
             em.set_author(name='Profile Picture')
             em.set_image(url=av)
-            await ctx.send(embed=em)    
+            await ctx.send(embed=em) 
+            
     
 if not os.environ.get('TOKEN'):
    print("no token found REEEE!")
