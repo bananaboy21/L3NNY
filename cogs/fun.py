fdb419dd6e7aacbba480dbb2ceb95d987650064f
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-05 16:58:12 -0600

    just a test command

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,12 +3,11 @@ import sys
 import os
 import io
 import asyncio
-import ezjson
 from discord.ext import commands
 
 
 class mod:
-    def __init__(self, bot):
+    def init(self, bot):
         self.bot = bot
 
     @commands.command()
@@ -17,9 +16,11 @@ class mod:
         """test"""
         try:
             await user.send(msg)
-            await ctx.message.delete()            
+            await ctx.message.delete()
             await ctx.send("works")
         except discord.ext.commands.MissingPermissions:
             await ctx.send("rip you dont got perms")
         except:
             await ctx.send("It did not make it.")
+def setup(bot):
+    bot.add_cog(UrCogClassName(bot))
