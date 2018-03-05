1f3ea0ba0ff0738de17362625b3bd561eb9754ac
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-05 16:33:01 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,17 +46,7 @@ async def on_guild_remove(guild):
     em = discord.Embed(color=discord.Color(value=0xf44242))
     em.title = "L3NNY has left a server."
     em.description = f"Server: {guild}"
-    await lol.send(embed=em)
-
-@bot.command()
-async def bug(ctx, *, msg:str):
-    """Report a bug """
-    lol = bot.get_channel(420065184141541377)
-    color = discord.Color(value=0xffffff)
-    em=discord.Embed(color=color, title="Bug reported!")
-    em.description = f"Bug: {msg}"
-    em.set_footer(text=f"Bug sent by {ctx.message.author.name}")                         
-    await ctx.send("Bug reported! Thank you for the bug!")   
+    await lol.send(embed=em)   
 
 
 @bot.command()
