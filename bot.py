accfa6525a67502eb60a657ee476df1c602df58c
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-11 21:33:17 -0500

    i broke it xd

--- bot.py
+++ bot.py
@@ -22,8 +22,13 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('_'),description="T
 async def on_ready():
     print('Bot is online, and ready to ROLL!')
     while True:
-        await bot.change_presence(activity=discord.Game(name='_help"))
-        await bot.change_presence(activity=discord.Game(name='"))        
+        await bot.change_presence(game=discord.Game(name=f"with {len(bot.guilds)} servers!"))
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name="_help"))
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name="V 0.0.8"))
+        await asyncio.sleep(10)
+        
     
     
 @bot.event
