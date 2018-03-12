99de0b7b748766333b571418f206d30b541b864e
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-11 21:10:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,11 +22,11 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('_'),description="T
 async def on_ready():
     print('Bot is online, and ready to ROLL!')
     while True:
-        await bot.change_presence(game=discord.Game(name=f"with {len(bot.guilds)} servers!"))
+        await bot.change_presence(activity=discord.Activity(name=f"with {len(bot.guilds)} servers!"))
         await asyncio.sleep(10)
-        await bot.change_presence(game=discord.Game(name="_help"))
+        await bot.change_presence(activity=discord.Activity(name="_help"))
         await asyncio.sleep(10)
-        await bot.change_presence(game=discord.Game(name="V 0.0.8"))
+        await bot.change_presence(activity=discord.Activity(name="V 0.0.8"))
         await asyncio.sleep(10)
         
     
