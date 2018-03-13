8d1f266738d61781578b9abb7c0ebf681042b0bc
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-13 15:48:40 -0500

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -15,16 +15,16 @@ class mod:
         
     @commands.command()
     @commands.has_permissions(administrator = True)
-    async def dm(self, ctx, user: discord.Member, *, msg: str):
-        """Escort your DM to someone thru the bot. Usage: *dm [tag person] [msg]"""
+    async def msg(self, ctx, user: discord.Member, *, msg: str):
+        """Message a user as me!"""
         try:
             await user.send(msg)
             await ctx.message.delete()            
-            await ctx.send("SuccESS! Your DM has made it! :white_check_mark: ")
+            await ctx.send("It has made it. hehehe...")
         except commands.MissingPermissions:
-            await ctx.send("Aw, come on! You thought you could get away with DM'ing people without permissions.")
+            await ctx.send("rip. You dont have enough perms")
         except:
-            await ctx.send("Error :x:. Make sure your message is shaped in this way: *dm [tag person] [msg]")
+            await ctx.send("Error :x:. Make sure your message is shaped in this way: _msg [tag person] [message]")
             
             
 
