74866b314a81b4112adb2ade0814d94b941d281c
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-13 13:07:42 -0500

    idk what i added

--- bot.py
+++ bot.py
@@ -18,6 +18,13 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('_'),description="TheEmperor™'s Discord bot.\n\nHelp Commands",owner_id=250674147980607488)
 
 
+def cleanup_code(content):
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    return content.strip('` \n')
+
 
 @bot.event
 async def on_guild_join(guild):
