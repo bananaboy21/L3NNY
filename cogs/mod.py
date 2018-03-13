c745842e0a8f1e7c7d1b40a00f3b4edda3ef7b84
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-03-13 15:43:59 -0500

    created cogs. finally

---
+++ cogs/mod.py
@@ -0,0 +1,33 @@
+import discord
+import sys
+import os
+import io
+import asyncio
+import json
+import ezjson
+from discord.ext import commands
+
+
+class mod:
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+    @commands.command()
+    @commands.has_permissions(administrator = True)
+    async def dm(self, ctx, user: discord.Member, *, msg: str):
+        """Escort your DM to someone thru the bot. Usage: *dm [tag person] [msg]"""
+        try:
+            await user.send(msg)
+            await ctx.message.delete()            
+            await ctx.send("SuccESS! Your DM has made it! :white_check_mark: ")
+        except commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with DM'ing people without permissions.")
+        except:
+            await ctx.send("Error :x:. Make sure your message is shaped in this way: *dm [tag person] [msg]")
+            
+            
+
+def setup(bot): 
+    bot.add_cog(mod(bot))            
+            
